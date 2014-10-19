30fe4e084b2c1dc5a64f56fa07275fe368c12a10
oparamo <oscar.paramo@me.com>
2014-10-19 04:14:57 -0500

    added gmaps api and updated near me

--- app/src/com/parse/starter/CatalogActivity.java
+++ app/src/com/parse/starter/CatalogActivity.java
@@ -4,19 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class CatalogActivity extends Activity {
-	
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.catalog);
-	}
-	
-	@Override
-	public void onStart() {
-		super.onStart();
-	}
-	
-	@Override
-	public void onStop() {
-		super.onStart();
-	}
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
--- app/src/com/parse/starter/NearbyActivity.java
+++ app/src/com/parse/starter/NearbyActivity.java
@@ -1,10 +1,23 @@
 package com.parse.starter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseGeoPoint;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
 import android.app.Activity;
 import android.os.Bundle;
 
 public class NearbyActivity extends Activity {
+	// private variables
+	private ParseGeoPoint currentLocation;
+	private GPSTracker gps;
 	
+	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.nearby);
@@ -13,10 +26,62 @@ public class NearbyActivity extends Activity {
 	@Override
 	public void onStart() {
 		super.onStart();
+		gps = new GPSTracker(this);
+		// Get user location and adds entry when done.
+		if (gps.canGetLocation) {
+            currentLocation = new ParseGeoPoint(gps.getLatitude(), gps.getLongitude());
+            findNearby(currentLocation, 2);
+		} else {
+			//throw exception
+		}
 	}
 	
 	@Override
 	public void onStop() {
 		super.onStart();
+	}
+	
+	// finds nearby locations
+	public void findNearby(final ParseGeoPoint currentLocation, int radius) {
+		// creates a query and gives it a distance condition
+		ParseQuery<ParseObject> query = ParseQuery.getQuery("Entry");
+		query.whereWithinMiles("Location", currentLocation, radius);
+		query.findInBackground(new FindCallback<ParseObject>() {
+	        public void done(List<ParseObject> objects, ParseException e) {
+	            if (e == null) {
+	            	// updates the objects list to be sorted by proximity to user
+	            	objects = sortPoints(objects, currentLocation);
+	            	
+	            	
+	            } else {
+	                //throw exception
+	            }
+	        }
+		});
+	}
+	
+	// orders the list of ParseGeoPoints
+	public List<ParseObject> sortPoints(List<ParseObject> objects, ParseGeoPoint currentLocation) {
+		List<ParseObject> sortedObjects= new ArrayList<ParseObject>();
+		/*double a = 0;
+		double b = 0;
+		
+		// iterates through the given objects
+		for (int i = 1; i < objects.size(); i++) {
+			int index = objects.get(i);
+			int j = i;
+			while (j > 0 && objects[j-1] > index) {
+				
+			}
+		}
+		
+		// returns a sorted list*/
+		return sortedObjects;
+	}
+	
+	// calculates distance between two ParseGeoPoints
+	public double calcDistance(ParseGeoPoint a, ParseGeoPoint b) {
+		// gets the two points and returns the distance between them
+		return Math.abs(Math.sqrt(Math.pow((b.getLatitude() - a.getLatitude()), 2) + Math.pow(b.getLongitude() - a.getLongitude(), 2)));
 	}
 }
